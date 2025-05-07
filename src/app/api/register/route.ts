36b71fcc64e56f96be26e0bbf58a0fb8e1a49953
qiongqiongyuren <qiongqiongyure@outlook.com>
2025-05-07 13:47:36 +0800

    可以上云端

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const prisma = new PrismaClient();
 
